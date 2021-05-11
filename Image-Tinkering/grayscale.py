a66232142b2e594ff0926c85223e9e4a49372728
NaYrA-IaR <56719715+NaYrA-IaR@users.noreply.github.com>
2021-05-11 15:13:14 +0530

    Grayscaling an image

---
+++ Image-Tinkering/grayscale.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Grayscale Image is an image with only one value for a pixel which represents black, white or 253 shades of gray.
+
+def convert_to_grayscale(data):
+
+    
+    row, column = data.shape[:2]
+    new_data = np.zeros((row,column))
+    
+    for r in range(row):
+        for c in range(column):
+            new_data[r][c] = data[r][c][0] + data[r][c][1] + data[r][c][2] # Only taking RGB values out of the RGBA format.
+
+    return new_data
--- Image-Tinkering/main.py
+++ Image-Tinkering/main.py
@@ -3,16 +3,20 @@ from matplotlib import pyplot as plt
 from binarizer import binarize
 from scaleup import scale
 import numpy as np
+from grayscale import convert_to_grayscale 
 
 
 image = img("image.png")
 
 #binarizing image
 threshold = 127
-new_data = binarize(image, threshold)
+binarized_image = binarize(image, threshold)
 
 #scaling image
-new_data = np.array(scale(image, 0.5))
+scaled_image = np.array(scale(image, 0.5))
 
-plt.imshow(new_data)
+# Grayscale Image
+gray_image = convert_to_grayscale(image)
+
+plt.imshow(gray_image, cmap="gray")
 plt.show()
